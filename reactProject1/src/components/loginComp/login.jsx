623a09a90169783e84c45b9988e580ae6d9ccdf8
sanjeev-yadavv <kumarsanjeev.sk575@gmail.com>
2024-10-08 22:38:37 +0530

    setting up state in login component

--- reactProject1/src/components/loginComp/login.jsx
+++ reactProject1/src/components/loginComp/login.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Button, Input } from "@chakra-ui/react";
 import "./login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   function handleSubmit(e) {
     e.preventDefault();
   }
   return (
     <Box>
       <form onSubmit={handleSubmit}>
-        <Input placeholder="customer email" />
-        <Input placeholder=" customer password" />
+        <Input
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="customer email"
+        />
+        <Input
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder=" customer password"
+        />
         <Box
           display="flex"
           alignItems="center"
